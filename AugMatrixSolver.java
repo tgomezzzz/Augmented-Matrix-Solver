7ab50deb9deb5a134a0b2043cc3de43c3ec4a94e
Tommy Gomez <tjk2132@columbia.edu>
2020-02-23 18:52:51 -0500

    fixed formatting errors with tabs

--- AugMatrixSolver.java
+++ AugMatrixSolver.java
@@ -5,13 +5,12 @@ public class AugMatrixSolver {
 	public static void main(String[] bears) {
 
 		Matrix m = new Matrix();
-		
-		int steps = 0;
-		while (!m.isRREF() && steps++ < 10){
+
+		while (!m.isRREF()) {
 			m.printMatrix();
 		}
 
 		m.interpretRREFMatrix();
 
 	}
-}	
+}
